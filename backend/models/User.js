134ff09f8eac56548d939f75ac76f8c0f557ef21
TuanVi <vi224644@student.nctu.edu.vn>
2025-10-21 15:23:43 +0700

    Update schema user

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength:6 },
+  role: { type: String, enum: ["user", "admin"], default: "user" },
+  avatar: { type: String, default: "default.png" }
 });
-
 module.exports = mongoose.model("User", userSchema);
-
